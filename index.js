5fc0ac234054145a479f1803ba87bdb56a2b7617
nuggiexx <hernashley98@gmail.com>
2017-12-30 20:23:12 -0800

    Updated search

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ function loadData() {
   let theSearch = $('#searchInfo').val();
   let wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + theSearch + '&format=json&callback=wikiCallback';
   let $wikiElem = $('#wiki-articles');
-
-  let wikiRequestTimeout = setTimeout (function() {
-    $wikiElem.text("Oops! Articles failed to load. Try again!");
-  }, 2000);
+  
+  $wikiElem.html('');
 
   $.ajax({
     url: wikiUrl,
     dataType: 'jsonp',
     success: function(data) {
-      $wikiElem.html('Articles about ' + theSearch + '<br/>');
-      let results = data[1];
-      for (let n = 0; n < results.length; n++) {
-        let moreResults = results[n];
-        let anotherUrl = 'http://en.wikipedia.org/wiki/' + moreResults;
-        $wikiElem.append('<a href="' + anotherUrl +'" target="_blank">' + moreResults + '</a><br/>');
-      };
-      clearTimeout(wikiRequestTimeout);
+      if (data[1].length > 1) {
+        $wikiElem.html('Articles about ' + theSearch + '<br/>');
+        let results = data[1];
+
+        // Loop for creating list of articles
+        for (let n = 0; n < results.length; n++) {
+          let moreResults = results[n];
+          let anotherUrl = 'http://en.wikipedia.org/wiki/' + moreResults;
+          $wikiElem.append('<a href="' + anotherUrl +'" target="_blank">' + moreResults + '</a><br/>');
+        };
+      } else {
+        $wikiElem.html('<div>Whoops! Please try another search.</div>');
+      }
+    },
+    error: function(data) {
+      $wikiElem.html('<div>Something went wrong</div>');
     }
   });
 };
